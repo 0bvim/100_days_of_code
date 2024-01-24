10f7463358e231705390605bf851050ddec87cd5
nivicius <niviciusdev@gmail.com>
2024-01-23 23:22:56 -0300

    updated
    insert fstring instead use plus concat in print function

--- BMI_calculator/bmi_calc.py
+++ BMI_calculator/bmi_calc.py
@@ -4,4 +4,6 @@ weight = input("Type your weight: ")
 height = float(input("Type your height: "))
 bmi = int(weight) / float((height ** 2))
 
-print ("Your BMI is " + str(bmi))
+print (f"Your BMI is {bmi}")
+
+# F strings allow you print variables just using curly braces envolving the variable name
