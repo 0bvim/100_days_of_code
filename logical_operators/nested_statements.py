372c060c0e78fbd796a238f682e21b9f5fee9660
nivicius <niviciusdev@gmail.com>
2024-03-21 10:50:45 -0300

    feat: updated rollecoaster program

--- logical_operators/nested_statements.py
+++ logical_operators/nested_statements.py
@@ -1,14 +1,24 @@
 print("Welcome to the rollecoaster")
 height = int(input("Type your height in centimeters: "))
 
+bill = 0
 if height >= 120:
     print("Nice, you can ride dude!")
     age = int(input("Tell me your age and I'll tell you the price of ticket: "))
     if age > 18:
-        print("$12 is the price of ticket")
+        bill = 12
+        print(f"{bill} is the price of ticket")
     elif age >= 12 and age <= 18:
-        print("$7 is the price of ticket")
+        bill = 7
+        print(f"{bill} is the price of ticket")
     else:
-        print("$5 is the price of ticket")
+        bill = 5
+        print(f"{bill} is the price of ticket")
+
+    want_photo = input("Do you want a photo taken? Y or N. ")
+    if want_photo == "Y":
+        bill += 3
+    print(f"Your final bill is {bill}")
+
 else:
     print("Sorry, you can't ride")
