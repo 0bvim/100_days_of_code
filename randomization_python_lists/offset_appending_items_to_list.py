13ed7a42d44da37cee549cfcd92923f4ea683d1c
nivicius <niviciusdev@gmail.com>
2024-03-23 22:28:18 -0300

    feat: lists introduction
    append, extend function

---
+++ randomization_python_lists/offset_appending_items_to_list.py
@@ -0,0 +1,15 @@
+states = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+    "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+    "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas",
+    "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",
+    "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota",
+    "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+    "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
+states.append("Viniland")
+states.extend(["Steteland", "Lilaland"])
+for i in states:
+    print(i)
