110365025a8d9b9bca3d61082d9d57c94b29273e
nivicius <niviciusdev@gmail.com>
2024-03-23 21:23:16 -0300

    feat: randomization function and modules
    how to import your own module too...

---
+++ randomization_python_lists/random_module.py
@@ -0,0 +1,13 @@
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+print(my_module.pi)
+
+random_float = random.random()
+print(random_float)
+
+random_range = random.uniform(1, 5)
+print(random_range)
+
