e1373a0662336d491d49b3d3e1f7979bad350f98
nivicius <niviciusdev@gmail.com>
2024-06-24 00:26:05 -0300

    feat: start the dictionary

---
+++ day09/dictionary.py
@@ -0,0 +1,10 @@
+programming_dict = {
+    "Bug": "I don't like of yellow submarine music.",
+    "Error": "Misleading identation",
+    "Loop": "Loop detected",
+}
+
+# the value to retrieve the value is the key like example below
+print(programming_dict["Bug"])
+print(programming_dict["Error"])
+print(programming_dict["Loop"])
