7be24a6baf379bdfbfa999b28eecc4e5ce85d738
nivicius <niviciusdev@gmail.com>
2024-06-24 00:31:03 -0300

    feat: operations with dictionary

--- day09/dictionary.py
+++ day09/dictionary.py
@@ -8,3 +8,18 @@ programming_dict = {
 print(programming_dict["Bug"])
 print(programming_dict["Error"])
 print(programming_dict["Loop"])
+
+# adding new value
+programming_dict["Hello"] = "Hello, how are you today?"
+print(programming_dict["Hello"])
+
+# creating new empty dict
+empty_dict = {}
+
+# to wipe an existing dictionary
+programming_dict = {}
+print(programming_dict)
+
+# editing an existing entry
+programming_dict["Bug"] = "Not more a bug"
+print(programming_dict["Bug"])
