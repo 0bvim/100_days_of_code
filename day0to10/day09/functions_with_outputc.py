732536ea8beafd8d06e195617373a521fd03ede2
nivicius <niviciusdev@gmail.com>
2024-06-25 00:31:20 -0300

    feat: multiple returns

--- day0to10/day09/functions_with_outputc.py
+++ day0to10/day09/functions_with_outputc.py
@@ -1,7 +1,19 @@
+# def format_name(f_name, l_name):
+#     ff_name = f_name.title()
+#     fl_name = l_name.title()
+#
+#     return f"{ff_name} {fl_name}"
+#
+# print(format_name("estephany", "freiTaS"))
+
+# multiple returns
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "Wrong input, bye."
     ff_name = f_name.title()
     fl_name = l_name.title()
 
     return f"{ff_name} {fl_name}"
 
-print(format_name("estephany", "freiTaS"))
+
+print(format_name(input("First name: "), input("Last Name: ")))
