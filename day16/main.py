1b41aef856d06f7f0ca4cde76ab2fce186434eb6
nivicius <niviciusdev@gmail.com>
2024-07-25 09:09:40 -0300

    feat: start fun with turtle

--- day16/main.py
+++ day16/main.py
@@ -1,1 +1,8 @@
-#this module introduce oop with python
+#this module introduce oop with python lib 'turtle'
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
