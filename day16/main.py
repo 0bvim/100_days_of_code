afbeca3de4a762f8fdfbe29538fd0e95d7fee1f8
nivicius <niviciusdev@gmail.com>
2024-07-26 01:46:08 -0300

    feat: venv creation

--- day16/main.py
+++ day16/main.py
@@ -1,18 +1,24 @@
 #this module introduce oop with python lib 'turtle'
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red", "brown")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red", "brown")
+# timmy.forward(100)
 
-for steps in range(100):
-	for c in ('blue', 'red', 'green'):
-		timmy.color(c)
-		timmy.forward(steps)
-		timmy.right(30)
+# for steps in range(100):
+# 	for c in ('blue', 'red', 'green'):
+# 		timmy.color(c)
+# 		timmy.forward(steps)
+# 		timmy.right(30)
 
-my_screen = Screen()
-my_screen.exitonclick()
-print(my_screen.canvheight)
+# my_screen = Screen()
+# my_screen.exitonclick()
+# print(my_screen.canvheight)
+
+x = PrettyTable() 
+x.add_column("City name", ["Name", "Phone", "Last Name"])
+
+print(x)
