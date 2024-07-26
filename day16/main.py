0e0af6cfe3694c751bf93e9a80eaf1a6f0dd02c9
nivicius <niviciusdev@gmail.com>
2024-07-26 01:16:52 -0300

    feat: added circular walk too

--- day16/main.py
+++ day16/main.py
@@ -7,6 +7,12 @@ timmy.shape("turtle")
 timmy.color("red", "brown")
 timmy.forward(100)
 
+for steps in range(100):
+	for c in ('blue', 'red', 'green'):
+		timmy.color(c)
+		timmy.forward(steps)
+		timmy.right(30)
+
 my_screen = Screen()
 my_screen.exitonclick()
 print(my_screen.canvheight)
