d889b14439911db2f2932064b1407c1b1d4326e4
nivicius <niviciusdev@gmail.com>
2024-07-26 01:14:36 -0300

    feat: turtle walk

--- day16/main.py
+++ day16/main.py
@@ -2,7 +2,11 @@
 from turtle import Turtle, Screen
 
 timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red", "brown")
+timmy.forward(100)
 
 my_screen = Screen()
-print(my_screen.canvheight)
 my_screen.exitonclick()
+print(my_screen.canvheight)
