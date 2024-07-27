4beaebe0a0dab30d622497f80163e7a099d1a325
0bvim <niviciusdev@gmail.com>
2024-07-27 00:21:02 -0300

    feat: added another column and separated files

---
+++ day16/pretty_table.py
@@ -0,0 +1,6 @@
+from prettytable import PrettyTable;
+
+x = PrettyTable() 
+x.add_column("Pokename", ["Xipaku", "Squirtle", "Charmander"])
+x.add_column("Type", ["Eletric", "Water", "Fire Name"])
+print(x)
