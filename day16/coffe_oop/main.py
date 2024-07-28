760787c9283476a1f65239a595c53b7578b17064
nivicius <niviciusdev@gmail.com>
2024-07-28 04:07:13 -0300

    feat: coffe machine with oop

--- day16/coffe_oop/main.py
+++ day16/coffe_oop/main.py
@@ -2,5 +2,25 @@ from menu import Menu, MenuItem
 from coffe_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-machine = CoffeeMaker()
-machine.report()
+money_machine = MoneyMachine()
+coffe_machine = CoffeeMaker()
+menu = Menu()
+
+coffe_machine.report()
+money_machine.report()
+
+is_on = True
+
+while is_on:
+	options = menu.get_items()
+	choice = input(f"What would you like? ({options}): ")
+	if choice == "off":
+		is_on = False
+	elif choice == "report":
+		coffe_machine.report()
+		money_machine.report()
+	else:
+		drink = menu.find_drink(choice)
+		if coffe_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+			coffe_machine.make_coffee(drink)
+
