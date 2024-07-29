e0cb6f29a74b270d9ec4c32d06333ebcf1cbad8d
nivicius <niviciusdev@gmail.com>
2024-07-29 01:03:49 -0300

    feat: added a user follow each other

---
+++ day17/adding_methods.py
@@ -0,0 +1,8 @@
+class Car:
+	
+	def __init__(self):
+		self.seats = 4
+		self.color = "black"
+
+	def enter_race_mode(self):
+		self.seats = 2
--- day17/main.py
+++ day17/main.py
@@ -3,7 +3,19 @@ class User:
 	def __init__(self, user_id, username):
 		self.id = user_id
 		self.username = username
-		print(f"At position {self.id}: User {self.username} created.")
+		self.followers = 0
+		self.following = 0
+	
+	def follow(self, user):
+		user.followers += 1
+		self.following += 1
 
 user_1 = User("001", "Stete")
 user_2 = User("002", "Lila")
+user_3 = User("003", "Nivi")
+
+user_1.follow(user_2)
+user_1.follow(user_3)
+
+print(user_1.followers)
+print(user_1.following)
