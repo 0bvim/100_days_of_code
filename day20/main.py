933173425fae35442387a3f65692bf85cc0da884
nivicius <niviciusdev@gmail.com>
2024-08-11 23:54:23 -0300

    feat: basic snake game without controls

--- day20/main.py
+++ day20/main.py
@@ -10,18 +10,23 @@ snake_pieces = []
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
 for position in starting_position:
-	new_piece = Turtle(shape="square")
-	new_piece.penup()
-	new_piece.color('white')
-	new_piece.goto(position)
-	snake_pieces.append(new_piece)
+    new_piece = Turtle(shape="square")
+    new_piece.penup()
+    new_piece.color('white')
+    new_piece.goto(position)
+    snake_pieces.append(new_piece)
 
 game_is_on = True
 
 while game_is_on:
-	screen.update()
-	time.sleep(0.1)
-	for piece in snake_pieces:
-		piece.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    for piece_num in range(len(snake_pieces) - 1, 0, -1):
+        new_x = snake_pieces[piece_num - 1].xcor()
+        new_y = snake_pieces[piece_num - 1].ycor()
+        snake_pieces[piece_num].goto(new_x, new_y)
+
+    snake_pieces[0].forward(20)
 
 screen.exitonclick()
