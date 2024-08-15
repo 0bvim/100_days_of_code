f4f2ab82e58574165acf13e2bf565ad1bf699503
nivicius <niviciusdev@gmail.com>
2024-08-15 11:06:39 -0300

    feat: both paddles implemented with moviments and so forth

--- day22/main.py
+++ day22/main.py
@@ -6,17 +6,28 @@ def screen_init():
 	screen.bgcolor("black")
 	screen.setup(width=800, height=600)
 	screen.title("Welcome to the Pongle")
+	screen.tracer(0) # it is to turn off animation for creation of paddles
 	return screen
+
+def screen_listen(right_paddle, left_paddle):
+	screen.listen()
+	screen.onkey(right_paddle.go_up, "Up")
+	screen.onkey(right_paddle.go_down, "Down")
+	screen.onkey(left_paddle.go_up, "w")
+	screen.onkey(left_paddle.go_down, "s")
 
 # initialize screen
 screen = screen_init()
 
 #put paddles
-paddle = Paddle()
+left_paddle = Paddle("left")
+right_paddle = Paddle("right")
 
 # listen events
-screen.listen()
-screen.onkey(paddle.go_up, "Up")
-screen.onkey(paddle.go_down, "Down")
+screen_listen(right_paddle, left_paddle)
+
+game_is_on = True
+while game_is_on:
+	screen.update()
 
 screen.exitonclick()
--- day22/paddle.py
+++ day22/paddle.py
@@ -1,14 +1,25 @@
 from turtle import Turtle
 
+left = -350
+right = 350
+
 class Paddle(Turtle):
 	
-	def __init__(self):
+	def __init__(self, position):
 		super().__init__()
 		self.shape("square")
 		self.color("red")
 		self.shapesize(stretch_wid=5, stretch_len=1)
 		self.penup()
-		self.goto(350, 0)
+		self.goto(self._check_side(position))
+
+	def _check_side(self, position):
+		if position == "left":
+			return (-350, 0)
+		elif position == "right":
+			return (350, 0)
+		else:
+			raise ValueError("Positio must be 'right' or 'left'")
 
 	def go_up(self):
 		if self.ycor() == (600/2):
