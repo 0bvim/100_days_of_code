6f843f0e69a4d3ba24aee3032d8922ca90e0ac40
nivicius <niviciusdev@gmail.com>
2024-08-27 22:50:36 -0300

    feat: using dict and dataframe with pandas

--- day25/main.py
+++ day25/main.py
@@ -15,5 +15,31 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data)) # dataframe
-print(data["temp"]) # series (single column)
+# print(type(data)) # dataframe
+# print(data["temp"]) # series (single column)
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# get data in columns
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+# create a brand new data frame
+
+data_ditc = {
+        "students": ["Stete", "Lila", "Nivi"],
+        "scores": [100, 101, 100]
+}
+
+new_frame = pandas.DataFrame(data_ditc)
+print(new_frame)
+
+# convert to CSV file
+
+new_frame.to_csv("new_frame.csv")
